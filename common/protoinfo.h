e3f91080568f3fd8e9d46cb17a3a8c138a98dc94
agaojs <a@gaojs.cn>
2019-11-01 17:11:09 +0800

    网络分为5层，分别是：物理层，链路层(ETHER)，网络层(ARP/IP)，传输层(TCP/UDP/ICMP)，应用层(HTTP)。

--- common/protoinfo.h
+++ common/protoinfo.h
@@ -1,70 +1,56 @@
 //////////////////////////////////////////////////
 // protoinfo.h文件
-
 /*
-
 定义协议格式
 定义协议中使用的宏
-
  */
-
-
 #ifndef __PROTOINFO_H__
 #define __PROTOINFO_H__
 
-
 #define ETHERTYPE_IP    0x0800
 #define ETHERTYPE_ARP   0x0806
-
-typedef struct _ETHeader         // 14字节的以太头
+typedef struct _ETHeader // 14字节的以太头
 {
-	UCHAR	dhost[6];			// 目的MAC地址destination mac address
-	UCHAR	shost[6];			// 源MAC地址source mac address
-	USHORT	type;				// 下层协议类型，如IP（ETHERTYPE_IP）、ARP（ETHERTYPE_ARP）等
-} ETHeader, *PETHeader;
-
+	UCHAR dhost[6];	// 目的MAC地址destination mac address
+	UCHAR shost[6];	// 源MAC地址source mac address
+	USHORT type;	// 下层协议类型，如IP（ETHERTYPE_IP）、ARP（ETHERTYPE_ARP）等
+} ETHeader, * PETHeader;
 
 #define ARPHRD_ETHER 	1
-
 // ARP协议opcodes
-#define	ARPOP_REQUEST	1		// ARP 请求	
-#define	ARPOP_REPLY		2		// ARP 响应
-
-
-typedef struct _ARPHeader		// 28字节的ARP头
+#define	ARPOP_REQUEST	1	// ARP 请求	
+#define	ARPOP_REPLY		2	// ARP 响应
+typedef struct _ARPHeader	// 28字节的ARP头
 {
-	USHORT	hrd;				//	硬件地址空间，以太网中为ARPHRD_ETHER
-	USHORT	eth_type;			//  以太网类型，ETHERTYPE_IP ？？
-	UCHAR	maclen;				//	MAC地址的长度，为6
-	UCHAR	iplen;				//	IP地址的长度，为4
-	USHORT	opcode;				//	操作代码，ARPOP_REQUEST为请求，ARPOP_REPLY为响应
-	UCHAR	smac[6];			//	源MAC地址
-	UCHAR	saddr[4];			//	源IP地址
-	UCHAR	dmac[6];			//	目的MAC地址
-	UCHAR	daddr[4];			//	目的IP地址
-} ARPHeader, *PARPHeader;
-
+	USHORT	hrd;		//	硬件地址空间，以太网中为ARPHRD_ETHER
+	USHORT	eth_type;	//  以太网类型，ETHERTYPE_IP ？？
+	UCHAR	maclen;		//	MAC地址的长度，为6
+	UCHAR	iplen;		//	IP地址的长度，为4
+	USHORT	opcode;		//	操作代码，ARPOP_REQUEST为请求，ARPOP_REPLY为响应
+	UCHAR	smac[6];	//	源MAC地址
+	UCHAR	saddr[4];	//	源IP地址
+	UCHAR	dmac[6];	//	目的MAC地址
+	UCHAR	daddr[4];	//	目的IP地址
+} ARPHeader, * PARPHeader;
 
 // 协议
 #define PROTO_ICMP    1
 #define PROTO_IGMP    2
 #define PROTO_TCP     6
 #define PROTO_UDP     17
-
-typedef struct _IPHeader		// 20字节的IP头
+typedef struct _IPHeader	// 20字节的IP头
 {
-    UCHAR     iphVerLen;      // 版本号和头长度（各占4位）
-    UCHAR     ipTOS;          // 服务类型 
-    USHORT    ipLength;       // 封包总长度，即整个IP报的长度
-    USHORT    ipID;			  // 封包标识，惟一标识发送的每一个数据报
-    USHORT    ipFlags;	      // 标志
-    UCHAR     ipTTL;	      // 生存时间，就是TTL
-    UCHAR     ipProtocol;     // 协议，可能是TCP、UDP、ICMP等
-    USHORT    ipChecksum;     // 校验和
-    ULONG     ipSource;       // 源IP地址
-    ULONG     ipDestination;  // 目标IP地址
-} IPHeader, *PIPHeader; 
-
+	UCHAR     iphVerLen;	// 版本号和头长度（各占4位）
+	UCHAR     ipTOS;		// 服务类型 
+	USHORT    ipLength;		// 封包总长度，即整个IP报的长度
+	USHORT    ipID;			// 封包标识，惟一标识发送的每一个数据报
+	USHORT    ipFlags;		// 标志
+	UCHAR     ipTTL;		// 生存时间，就是TTL
+	UCHAR     ipProtocol;	// 协议，可能是TCP、UDP、ICMP等
+	USHORT    ipChecksum;	// 校验和
+	ULONG     ipSource;		// 源IP地址
+	ULONG     ipDestination;	// 目标IP地址
+} IPHeader, * PIPHeader;
 
 // 定义TCP标志
 #define   TCP_FIN   0x01
@@ -75,8 +61,7 @@ typedef struct _IPHeader		// 20字节的IP头
 #define   TCP_URG   0x20
 #define   TCP_ACE   0x40
 #define   TCP_CWR   0x80
-
-typedef struct _TCPHeader		// 20字节的TCP头
+typedef struct _TCPHeader	// 20字节的TCP头
 {
 	USHORT	sourcePort;			// 16位源端口号
 	USHORT	destinationPort;	// 16位目的端口号
@@ -96,7 +81,7 @@ typedef struct _TCPHeader		// 20字节的TCP头
 	USHORT	windows;			// 16位窗口大小
 	USHORT	checksum;			// 16位校验和
 	USHORT	urgentPointer;		// 16位紧急数据偏移量 
-} TCPHeader, *PTCPHeader;
+} TCPHeader, * PTCPHeader;
 
 typedef struct _UDPHeader
 {
@@ -104,7 +89,7 @@ typedef struct _UDPHeader
 	USHORT			destinationPort;// 目的端口号		
 	USHORT			len;			// 封包长度
 	USHORT			checksum;		// 校验和
-} UDPHeader, *PUDPHeader;
+} UDPHeader, * PUDPHeader;
 
 #endif // __PROTOINFO_H__
 
