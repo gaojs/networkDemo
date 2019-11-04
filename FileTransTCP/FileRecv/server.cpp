87c107050e9cbbaac26fd30b1197265cd7a10bd6
agaojs <a@gaojs.cn>
2019-11-04 17:08:47 +0800

    服务端，接收文件，客户端发送文件。用WinSock2.h

---
+++ FileTransTCP/FileRecv/server.cpp
@@ -0,0 +1,93 @@
+#include<winsock2.h>
+#include<windows.h>
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	cout << "服务器已开启" << endl;
+	WSADATA wsaData = { 0 };
+	WORD wVersionRequested = MAKEWORD(2, 2);
+	int nRet = WSAStartup(wVersionRequested, &wsaData);
+	cout << " WSAStartup()=" << nRet << endl;
+	if (nRet != 0)
+	{
+		return 0;
+	}
+	if (LOBYTE(wsaData.wVersion) != 2 ||
+		HIBYTE(wsaData.wVersion) != 2)
+	{
+		WSACleanup();
+		return 0;
+	}
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+	cout << " socket()=" << sock << endl;
+	SOCKADDR_IN addr = { 0 }, addrClient = { 0 };
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(6000);
+	addr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
+	nRet = bind(sock, (SOCKADDR*)&addr, sizeof(SOCKADDR));
+	cout << " bind()=" << nRet << endl;
+	nRet = listen(sock, 2);
+	cout << " listen()=" << nRet << endl;
+
+	int len = sizeof(SOCKADDR);
+	SOCKET sockClient = accept(sock, (SOCKADDR*)&addrClient, &len);
+	cout << " accept() sockClient=" << sockClient << endl;
+	cout << "connect successfull" << endl << " data is recving" << endl;
+	cout << " you will receive a file called final.txt, please waiting…" << endl;
+	nRet = send(sockClient, "connect success!", strlen("connect success!") + 1, 0);
+	static int a = 1;
+	long int size = 0;
+	int sizenum = 1;
+	int value = 1;
+	const int BUF_SIZE = 51;
+	char buf[BUF_SIZE] = { 0 };
+	FILE* fp = fopen("final.txt", "wb");
+	while (true)
+	{
+		if (a)
+		{
+			a--;
+			size += 1;
+			nRet = recv(sockClient, buf, 1, 0);
+			cout << " recv()=" << nRet << endl;
+			fwrite(buf, 1, 1, fp);
+			Sleep(500);
+		}
+		value = recv(sockClient, buf, BUF_SIZE - 1, 0);
+		cout << " recv()=" << value << endl;
+		size += value;
+		int showSize = size / (1024 * 1024);
+		if (showSize >= sizenum)
+		{
+			cout << " send " << showSize << " MB" << endl;
+			sizenum++;
+		}
+		if (value < BUF_SIZE - 1)
+		{
+			if (value == 2 && buf[0] == 'q' && buf[1] == 'O')
+			{
+				cout << " quit" << endl;
+				break;
+			}
+			buf[value] = '\0';
+			fwrite(buf, value, 1, fp);
+		}
+		else if (value == BUF_SIZE - 1)
+		{
+			buf[BUF_SIZE - 1] = '\0';
+			fwrite(buf, BUF_SIZE - 1, 1, fp);
+		}
+	}
+	nRet = closesocket(sock);
+	cout << " closesocket()=" << nRet << endl;
+	nRet = WSACleanup();
+	cout << " WSACleanup()=" << nRet << endl;
+	fclose(fp);
+	cout << " recv finish !" << endl;
+	cin.get();
+}
+
+
---
+++ FileTransTCP/FileSend/client.cpp
@@ -0,0 +1,113 @@
+#include<winsock2.h>
+#include<windows.h>
+#include<iostream>
+#include<string>
+using namespace std;
+
+const int BUF_SIZE = 1401;
+char buf[BUF_SIZE] = { 0 }, buf1[BUF_SIZE] = { 0 };
+int main(int argc, char* argv[])
+{
+	WSADATA wsaData = { 0 };
+	WORD wVersionRequested = MAKEWORD(2, 2);
+	int nRet = WSAStartup(wVersionRequested, &wsaData);
+	cout << " WSAStartup()=" << nRet << endl;
+	if (nRet != 0)
+	{
+		return 1;
+	}
+	if (LOBYTE(wsaData.wVersion) != 2 ||
+		HIBYTE(wsaData.wVersion) != 2)
+	{
+		nRet = WSACleanup();
+		return 2;
+	}
+
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+	cout << " socket()=" << sock << endl;
+	cout << " enter srever's address:";
+	char ip[15] = { 0 };
+	cin.getline(ip, sizeof(ip));
+	SOCKADDR_IN addr = { 0 };
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(6000);
+	addr.sin_addr.s_addr = inet_addr(ip);
+	nRet = connect(sock, (SOCKADDR*)&addr, sizeof(SOCKADDR));
+	cout << " connect()=" << nRet << endl;
+	char notice[20] = { 0 };
+	nRet = recv(sock, notice, 23, 0);
+	cout << " recv()=" << nRet << endl;
+	cout << notice << endl;
+
+	cout << " put in the filename which you want to send:";
+	char filename[20] = { 0 };
+	cin.getline(filename, sizeof(filename));
+	cout << endl << " the data is sending now!  please waiting¡­  " << endl;
+	FILE* fp = fopen(filename, "rb");
+	if (!fp)
+	{
+		cout << " the file is NOT exist!" << endl;
+		nRet = closesocket(sock);
+		cout << " closesocket()=" << nRet << endl;
+		nRet = WSACleanup();
+		cout << " WSACleanup()=" << nRet << endl;
+		return 3;
+	}
+	int sign = 0;
+	int i = 0;
+	while (true)
+	{
+		strcpy(buf1, buf);
+		sign = fread(buf, BUF_SIZE - 1, 1, fp);
+		if (sign == 1)
+		{
+			buf[BUF_SIZE - 1] = '\0';
+			nRet = send(sock, buf, BUF_SIZE - 1, 0);
+			cout << " send()=" << nRet << endl;
+		}
+		else
+		{
+			while (i < BUF_SIZE - 1)
+			{
+				if (buf1[i] == buf[i]
+					&& buf1[i + 1] == buf[i + 1]
+					&& buf1[i + 2] == buf[i + 2])
+				{
+					sign = -1;
+					fread(buf, i, 1, fp);
+					buf[i] = '\0';
+					nRet = send(sock, buf, i, 0);
+					cout << " send()=" << nRet << endl;
+					break;
+				}
+				else
+				{
+					i++;
+				}
+				if (i == BUF_SIZE - 1)
+				{
+					sign = -1;
+					fread(buf, i, 1, fp);
+					buf[i] = '\0';
+					nRet = send(sock, buf, i, 0);
+					cout << " send()=" << nRet << endl;
+				}
+			}
+		}
+		if (sign == -1)
+		{
+			Sleep(1000);
+			nRet = send(sock, "qO", 2, 0);
+			cout << " send()=" << nRet << endl;
+			break;
+		}
+	}
+	nRet = closesocket(sock);
+	cout << " closesocket()=" << nRet << endl;
+	nRet = WSACleanup();
+	cout << " WSACleanup()=" << nRet << endl;
+	fflush(stdin);
+	printf(" send finish !\n");
+	cin.get();
+	return 0;
+}
