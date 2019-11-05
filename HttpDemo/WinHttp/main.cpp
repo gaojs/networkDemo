4d098fb14b46206e01f081b9fb8b700a75237ab3
agaojs <a@gaojs.cn>
2019-11-05 17:15:15 +0800

    优化代码

--- HttpDemo/WinHttp/main.cpp
+++ HttpDemo/WinHttp/main.cpp
@@ -20,23 +20,30 @@ int main()
 		WINHTTP_NO_PROXY_BYPASS, 0);
 	// Specify an HTTP server.
 	if (hSession)
+	{
 		hConnect = WinHttpConnect(hSession, L"www.pubyun.com",
 			INTERNET_DEFAULT_HTTP_PORT, 0);
-			//INTERNET_DEFAULT_HTTPS_PORT, 0);
+	}
+	//INTERNET_DEFAULT_HTTPS_PORT, 0);
 	// Create an HTTP request handle.
 	if (hConnect)
+	{
 		hRequest = WinHttpOpenRequest(hConnect, L"GET", L"dyndns/getip",
-			NULL, WINHTTP_NO_REFERER,
-			WINHTTP_DEFAULT_ACCEPT_TYPES,
+			NULL, WINHTTP_NO_REFERER, WINHTTP_DEFAULT_ACCEPT_TYPES,
 			0);// WINHTTP_FLAG_SECURE);
+	}
 	// Send a request.
 	if (hRequest)
+	{
 		bResults = WinHttpSendRequest(hRequest,
 			WINHTTP_NO_ADDITIONAL_HEADERS, 0,
 			WINHTTP_NO_REQUEST_DATA, 0, 0, 0);
+	}
 	// End the request.
 	if (bResults)
+	{
 		bResults = WinHttpReceiveResponse(hRequest, NULL);
+	}
 	// Keep checking for data until there is nothing left.
 	if (bResults)
 	{
@@ -47,13 +54,15 @@ int main()
 			if (!WinHttpQueryDataAvailable(hRequest, &dwSize))
 				printf("Error %u in WinHttpQueryDataAvailable.\n",
 					GetLastError());
-			else
+			if (dwSize > 0)
 			{
+				printf("WinHttpQueryDataAvailable dwSize=%d\n", dwSize);
 				// Allocate space for the buffer.
 				pszOutBuffer = new char[dwSize + 1];
 				if (!pszOutBuffer)
 				{
 					printf("Out of memory\n");
+					bResults = FALSE;
 					dwSize = 0;
 				}
 				else
@@ -62,9 +71,14 @@ int main()
 					ZeroMemory(pszOutBuffer, dwSize + 1);
 					if (!WinHttpReadData(hRequest, (LPVOID)pszOutBuffer,
 						dwSize, &dwDownloaded))
+					{
 						printf("Error %u in WinHttpReadData.\n", GetLastError());
+						bResults = FALSE;
+					}
 					else
+					{
 						printf("%s", pszOutBuffer);
+					}
 					// Free the memory allocated to the buffer.
 					delete[] pszOutBuffer;
 				}
@@ -73,7 +87,9 @@ int main()
 	}
 	// Report any errors.
 	if (!bResults)
+	{
 		printf("Error %d has occurred.\n", GetLastError());
+	}
 	// Close any open handles.
 	if (hRequest) WinHttpCloseHandle(hRequest);
 	if (hConnect) WinHttpCloseHandle(hConnect);
